aa9a6d1031469e9a547455a637ef73cc01e6675c
Nika <weronikakoga@gmail.com>
2026-02-02 16:05:20 +0000

    Initial commit: Tailwind setup, custom favicon, clean CRA

---
+++ src/App.js
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-pink-100">
+      <h1 className="text-5xl font-extrabold text-pink-600 text-center">
+        Nika & Jasiu reunion
+      </h1>
+    </div>
+  );
+}
